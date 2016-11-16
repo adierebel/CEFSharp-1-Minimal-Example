348a83537c5ae3effbec4ff844fbb8b5573792a3
adierebel <adie.rebel@gmail.com>
2016-11-16 15:42:03 +0700

    Init

---
+++ FormMain.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.PanelBrowser = New System.Windows.Forms.Panel
+        Me.ButtonHTML = New System.Windows.Forms.Button
+        Me.TimerLoad = New System.Windows.Forms.Timer(Me.components)
+        Me.ButtonURL = New System.Windows.Forms.Button
+        Me.TextBoxURL = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'PanelBrowser
+        '
+        Me.PanelBrowser.Location = New System.Drawing.Point(0, 40)
+        Me.PanelBrowser.Name = "PanelBrowser"
+        Me.PanelBrowser.Size = New System.Drawing.Size(419, 327)
+        Me.PanelBrowser.TabIndex = 0
+        '
+        'ButtonHTML
+        '
+        Me.ButtonHTML.Location = New System.Drawing.Point(5, 5)
+        Me.ButtonHTML.Name = "ButtonHTML"
+        Me.ButtonHTML.Size = New System.Drawing.Size(86, 29)
+        Me.ButtonHTML.TabIndex = 1
+        Me.ButtonHTML.Text = "Load HTML"
+        Me.ButtonHTML.UseVisualStyleBackColor = True
+        '
+        'TimerLoad
+        '
+        Me.TimerLoad.Enabled = True
+        '
+        'ButtonURL
+        '
+        Me.ButtonURL.Location = New System.Drawing.Point(97, 5)
+        Me.ButtonURL.Name = "ButtonURL"
+        Me.ButtonURL.Size = New System.Drawing.Size(86, 29)
+        Me.ButtonURL.TabIndex = 2
+        Me.ButtonURL.Text = "Load URL"
+        Me.ButtonURL.UseVisualStyleBackColor = True
+        '
+        'TextBoxURL
+        '
+        Me.TextBoxURL.Location = New System.Drawing.Point(189, 10)
+        Me.TextBoxURL.Name = "TextBoxURL"
+        Me.TextBoxURL.Size = New System.Drawing.Size(230, 20)
+        Me.TextBoxURL.TabIndex = 3
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 399)
+        Me.Controls.Add(Me.TextBoxURL)
+        Me.Controls.Add(Me.ButtonURL)
+        Me.Controls.Add(Me.ButtonHTML)
+        Me.Controls.Add(Me.PanelBrowser)
+        Me.Name = "FormMain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PanelBrowser As System.Windows.Forms.Panel
+    Friend WithEvents ButtonHTML As System.Windows.Forms.Button
+    Friend WithEvents TimerLoad As System.Windows.Forms.Timer
+    Friend WithEvents ButtonURL As System.Windows.Forms.Button
+    Friend WithEvents TextBoxURL As System.Windows.Forms.TextBox
+
+End Class
---
+++ FormMain.vb
@@ -0,0 +1,45 @@
+﻿Imports CefSharp
+Imports CefSharp.WinForms
+Public Class FormMain
+    Dim WebView As CefSharp.WinForms.WebView
+    Public Sub New()
+        InitializeComponent()
+        Dim WebViewSetting As New CefSharp.BrowserSettings
+        WebView = New WebView(String.Empty, WebViewSetting)
+        WebView.Dock = DockStyle.Fill
+        PanelBrowser.Controls.Add(WebView)
+        PanelBrowser.Width = Me.Width
+        PanelBrowser.Height = Me.Height - 40
+        PanelBrowser.Top = 40
+        PanelBrowser.Left = 0
+        WebView.RegisterJsObject("regJS", New regJS())
+    End Sub
+    Private Sub FormMain_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        PanelBrowser.Width = Me.Width
+        PanelBrowser.Height = Me.Height
+    End Sub
+    Private Sub FormMain_ResizeEnd(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.ResizeEnd
+        PanelBrowser.Width = Me.Width
+        PanelBrowser.Height = Me.Height
+    End Sub
+    Private Sub FormMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    End Sub
+    Private Sub ButtonHTML_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonHTML.Click
+        WebView.LoadHtml("<html><body style='margin:0;padding:0;'><br /><br /><h1><center>Hi, HTML Loaded !</center></h1><script type='text/javascript'>regJS.helloJS('halo dunia !');</script ></body></html>", "about:blank")
+    End Sub
+    Private Sub TimerLoad_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TimerLoad.Tick
+        If WebView.IsBrowserInitialized = True Then
+            WebView.LoadHtml("<html><body><h1>Test</h1></body></html>", "about:blank")
+            TimerLoad.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ButtonURL_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonURL.Click
+        WebView.Load(TextBoxURL.Text)
+    End Sub
+End Class
+Public Class regJS
+    Public Sub helloJS(ByVal text As String)
+        MsgBox("hit from JS -> " & text)
+    End Sub
+End Class
